ef330e7ea74ce618ec995ba7d80e10914db57ea7
Joseph Nguyen <joseph.h.nguyen@sjsu.edu>
2025-04-25 17:40:52 -0700

    Implement user auth and make dashboard

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,16 +1,26 @@
-import { CalendarClock, Plus } from 'lucide-react'
-import React from 'react'
-import { Button } from './ui/button'
-import Link from 'next/link'
+"use client"
+
+import { Leaf } from "lucide-react"
+import { Button } from "./ui/button"
+import Link from "next/link"
+import { SignedIn, SignedOut, SignInButton, useUser, UserButton } from "@clerk/nextjs"
+import { CustomUserButton } from "./CustomUserButton"
 
 const Navbar = () => {
-   return (
-      <header className="pl-5 pr-5 w-full sticky top-0 z-10 border-b bg-background/95 backdrop-blur">
-        <div className="flex h-16 items-center justify-between py-4">
-          <div className="flex items-center gap-2">
-            <CalendarClock className="h-6 w-6 text-primary" />
-            <h1 className="text-xl font-bold">DontDump TheYums</h1>
+  const { user } = useUser()
+  const firstName = user?.firstName || ""
+  const lastName = user?.lastName || ""
+
+  return (
+    <header className="pl-5 pr-5 w-full sticky top-0 bg-amber-50 z-50 border-b ">
+      <div className="flex h-16 items-center justify-between py-4">
+        <div className="flex items-center gap-2">
+          <div className="bg-green-600 p-2 rounded-full">
+            <Leaf className="h-6 w-6 text-white" />
           </div>
+          <h1 className="text-xl font-bold">DontDump TheYums</h1>
+        </div>
+        <SignedIn>
           <nav className="flex items-center gap-4">
             <Button variant="ghost" size="sm" asChild>
               <Link href="/">Dashboard</Link>
@@ -24,10 +34,20 @@ const Navbar = () => {
             <Button variant="ghost" size="sm" asChild>
               <Link href="/donate">Donate</Link>
             </Button>
+            <CustomUserButton />
+            
           </nav>
-        </div>
-      </header>
-   )
+        </SignedIn>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button className="bg-green-600 hover:bg-green-700 text-white px-6 rounded-full flex items-center gap-2 transition-all duration-300 shadow-sm hover:shadow-md">
+              Sign In
+            </Button>
+          </SignInButton>
+        </SignedOut>
+      </div>
+    </header>
+  )
 }
 
 export default Navbar
